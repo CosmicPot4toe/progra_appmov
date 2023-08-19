7e71e557ccbc5ea193d9219198e3c59e539de857
CosmicPot4toe <ji.figueroa.lopez@gmail.com>
2023-08-18 23:29:11 -0400

    FORMS are a pain
    Finished up the login form
    Started the hope form, I'm in PAIN help

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,47 @@ export class HomePage {
 
   constructor() {}
 
+	// Shit for the Education Level thingy, can't doc ever lol
+	isOpen = false;
+	SelOpt = '';
+  public Options = [
+    {
+      name: 'EducLVL',
+      options: [
+        {
+          text: 'Basica',
+          value: 'B',
+        },
+        {
+          text: 'Media',
+          value: 'M',
+        },
+        {
+          text: 'Superior',
+          value: 'S',
+        },
+        {
+          text: 'Profesional',
+          value: 'P',
+        }
+      ]
+    }
+  ];
+  public buTONS = [
+    {
+      text: 'Cancel',
+      role: 'cancel'
+    },
+    {
+      text: 'Confirm',
+			role: 'confirm',
+      handler: (value: any) => {
+        // window.alert(`You selected: ${value.languages.value}`);
+				this.SelOpt= value.EducLVL.text;
+      },
+    }
+  ];
+  setOpen(Open: boolean) {
+    this.isOpen = Open;
+  }
 }
