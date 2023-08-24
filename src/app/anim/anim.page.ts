8f958de8945fc54fc055f14615c76557d5caa29a
NMPotato <ji.figueroa.lopez@gmail.com>
2023-08-24 10:35:10 -0400

    Thingssssss
    owo

--- src/app/anim/anim.page.ts
+++ src/app/anim/anim.page.ts
@@ -25,13 +25,15 @@ export class AnimPage {
 			.addElement(this.square.nativeElement)
 			.duration(1500)
 			.iterations(Infinity)
-      .fromTo('transform', 'translateX(0px)', 'translateX(250px)')
-      .fromTo('opacity', '1', '0.2');
+			.fromTo('transform', 'translateX(0px)', 'translateX(250px)')
+			.fromTo('opacity', '1', '0.2');
 	}
 	play(){
 		this.anim.play();
 	}
-
+	pause(){
+		this.anim.pause();
+	}
 	stop(){
 		this.anim.stop();
 	}
